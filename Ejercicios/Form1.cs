d10b5944a70ee4930eb019c779dacef7c47ee47e
MHRosales <20120208@santacecilia.edu.sv>
2022-05-04 15:19:28 -0500

    Form 1

--- Ejercicios/Form1.cs
+++ Ejercicios/Form1.cs
@@ -16,5 +16,20 @@ namespace Ejercicios
         {
             InitializeComponent();
         }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
