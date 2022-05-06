0ca6c635ecc7a1160ddbbc50038d92133c7c054d
MHRosales <20120208@santacecilia.edu.sv>
2022-05-06 17:52:53 -0500

    todo

--- Ejercicios/Form2.Designer.cs
+++ Ejercicios/Form2.Designer.cs
@@ -86,7 +86,7 @@
             this.textBox1.Location = new System.Drawing.Point(414, 150);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(306, 36);
+            this.textBox1.Size = new System.Drawing.Size(290, 36);
             this.textBox1.TabIndex = 18;
             this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
